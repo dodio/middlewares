df97fa0409f2d26a1162d99fc0e7cfe6ceb7e10d
dodio <dodio@users.noreply.github.com>
2016-07-14 23:46:52 +0800

    原有的功能不变，在res对象上提供一个mock方法，可以对传入的filepath进行mock

--- dev/mock.js
+++ dev/mock.js
@@ -18,53 +18,14 @@ function mockFactory(conf) {
   if(!urlPattern && typeof urlPattern != 'string'){
     throw new Error('please specify the url pattern(String).')
   }
-
+  router.use(mockMethod)
   router.use(urlPattern, mock)
 
   function mock(req, res, next) {
     var path = pathPrex == "" ? req.path : req.path.replace(prexReg, ''),
-      filePath = nodePath.join(dataPath, path),
-      ext = nodePath.extname(path)
+        filePath = nodePath.join(dataPath, path)
 
-    // mock send file
-    if (ext != "") {
-      return res.sendFile(filePath, function(err) {
-        if (err) {
-          // 失败了也再尝试将文件当作restrul的形式来mock
-          mockRestFul()
-        }
-      })
-    }
-      
-    mockRestFul()
-
-    function mockRestFul(){
-      filePath = filePath.replace(/\/$/, "")
-      console.log(filePath);
-      var method = req.method.toLowerCase(),
-        dirPath = filePath + "/index",
-        jsFile = parsePrefer(filePath, method, '.js'),
-        jsonFile = parsePrefer(filePath, method, '.json'),
-        jsDir = parsePrefer(dirPath, method, '.js'),
-        jsonDir = parsePrefer(dirPath, method, '.json')
-
-      if (jsFile) {
-        return require(jsFile)(req, res, next)
-
-      }
-      if (jsonFile) {
-        return res.sendFile(jsonFile)
-      }
-      if (jsDir) {
-        return require(jsDir)(req, res, next)
-
-      }
-      if (jsonDir) {
-        return res.sendFile(jsonDir)
-      }
-      
-      !!shouldSkipNotFound ? next() : next(new FaError('not found mock data', 404))
-    }
+    res.mock(filePath, shouldSkipNotFound)
   }
 
   return router
@@ -82,4 +43,52 @@ function parsePrefer(path, method, ext) {
     return file
   }
   return ""
+}
+
+
+function mockMethod (req, res, next) {
+  res.mock = mock
+
+  function mock(filePath, shouldSkipNotFound) {
+    var ext = nodePath.extname(filePath)
+    // mock send file
+    if (ext != "") {
+      return res.sendFile(filePath, function(err) {
+        if (err) {
+          // 失败了也再尝试将文件当作restrul的形式来mock
+          mockRESTful(filePath, shouldSkipNotFound)
+        }
+      })
+    }
+    mockRESTful(filePath, shouldSkipNotFound)
+  }
+
+  function mockRESTful(filePath, shouldSkipNotFound){
+    filePath = filePath.replace(/\/$/, "")
+    var method = req.method.toLowerCase(),
+      dirPath = filePath + "/index",
+      jsFile = parsePrefer(filePath, method, '.js'),
+      jsonFile = parsePrefer(filePath, method, '.json'),
+      jsDir = parsePrefer(dirPath, method, '.js'),
+      jsonDir = parsePrefer(dirPath, method, '.json')
+
+    if (jsFile) {
+      return require(jsFile)(req, res, next)
+
+    }
+    if (jsonFile) {
+      return res.sendFile(jsonFile)
+    }
+    if (jsDir) {
+      return require(jsDir)(req, res, next)
+
+    }
+    if (jsonDir) {
+      return res.sendFile(jsonDir)
+    }
+    
+    !!shouldSkipNotFound ? next() : next(new FaError('not found mock data', 404))
+  }
+
+  next()
 }
